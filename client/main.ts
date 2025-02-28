c82d75d7e49929fa164144fa1f9ea86d65d0ac3c
Peter Fajner <peter@pfaj.ca>
2025-02-27 22:05:09 -0400

    Enable ts

---
+++ client/main.ts
@@ -0,0 +1,16 @@
+import './style.css'
+import rocketLogo from '/rocket.png'
+import { DiscordSDK } from "@discord/embedded-app-sdk";
+
+// const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+
+// setupDiscordSdk().then(() => {
+//   console.log("Discord SDK is ready");
+// });
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <img src="${rocketLogo}" class="logo" alt="Discord" />
+    <h1>Hello, World!</h1>
+  </div>
+`;
